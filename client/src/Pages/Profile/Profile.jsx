e5b31b548938636b2887f0990e53c17daca97f9a
aniloli42 <aniloli42@gmail.com>
2021-10-20 20:04:46 +0545

    add: working on profile

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import defaultProfileImage from "./../../Assets/Images/profile-image.png"
 
 const Profile = () => {
-    return (
-      <>
-        <h2>Profile</h2>
-      </>
-    )
+  return (
+    <>
+      {/* section title */}
+      <h2>Profile</h2>
+
+      <div className="profile-container">
+        <div className="profile-image">
+          <img src={defaultProfileImage} alt="" />
+          {/* TODO: change true to isLogin Variable */}
+          {true && <button id="profileImageUpdater">Update Image</button>}
+        </div>
+
+        <div className="profile-info">
+          <div className="profile-name input-group">
+            <label htmlFor="profileName">Name</label>
+            <input type="text" id="profileName" name="profileName" />
+          </div>
+
+          <div className="input-group profile-blood-type">input</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Profile
