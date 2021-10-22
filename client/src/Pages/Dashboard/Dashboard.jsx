93d949ff2211859a672dd28ef5cdebe6adce38e8
aniloli42 <aniloli42@gmail.com>
2021-10-22 19:20:02 +0545

    fix: unwanted class remove for element

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,40 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Badge, ImportantMessage, RecentRequest } from '../../Components'
-import './dashboard.css'
-
+import React from "react"
+import { Link } from "react-router-dom"
+import { Badge, ImportantMessage, RecentRequest } from "../../Components"
+import "./dashboard.css"
 
 const Dashboard = () => {
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <ImportantMessage
+        message="Verify Your Email Address"
+        action="Verify"
+        isAction={true}
+      />
 
+      <div className="small-cards">
+        <Badge badgetitle="Blood Group" badgetext="B+" />
+        <Badge badgetitle="Total Donation" badgetext="8" />
+        <Badge badgetitle="Total Requests" badgetext="8" />
+        <Badge badgetitle="Pending Requests" badgetext="3" />
+      </div>
 
-    return (
-        <>
-            <h2>Dashboard</h2>
-            <ImportantMessage message="Verify Your Email Address" action="Verify" isAction={true} />
+      <article className="recent-container">
+        <header>
+          <h2>Recent Requests</h2>
+          <Link className="view-all-action-button" to="/requests">
+            View All Requests
+          </Link>
+        </header>
 
-            <div className="small-cards">
-
-                <Badge badgetitle='Blood Group' badgetext="B+" />
-                <Badge badgetitle='Total Donation' badgetext="8" />
-                <Badge badgetitle='Total Requests' badgetext="8" />
-                <Badge badgetitle='Pending Requests' badgetext="3" />
-
-            </div>
-
-            <article className="recent-container">
-                <header>
-                    <h2>Recent Requests</h2>
-                    <Link className="view-all-action-button button" to="/requests">View All Requests</Link>
-                </header>
-
-                <div className="recent-contents" >
-                    <RecentRequest bloodtype={'O+'} address={"butwal"} requestname="Sanjay" id={1} />
-                    <RecentRequest bloodtype={'B+'} address={"Dang"} requestname="Anil Oli" id={2} />
-                    <RecentRequest bloodtype={'B+'} address={"Dang"} requestname="Shovit Banjade" id={3} />
-                </div>
-            </article>
-        </>
-    )
+        <div className="recent-contents">
+          <RecentRequest
+            bloodtype={"O+"}
+            address={"butwal"}
+            requestname="Sanjay"
+            id={1}
+          />
+          <RecentRequest
+            bloodtype={"B+"}
+            address={"Dang"}
+            requestname="Anil Oli"
+            id={2}
+          />
+          <RecentRequest
+            bloodtype={"B+"}
+            address={"Dang"}
+            requestname="Shovit Banjade"
+            id={3}
+          />
+        </div>
+      </article>
+    </>
+  )
 }
 
 export default Dashboard
