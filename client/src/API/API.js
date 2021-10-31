498ee1002ffabd81da96fb83ccf753b3d6b7cdb6
aniloli42 <aniloli42@gmail.com>
2021-11-01 00:21:59 +0545

    fix: dotenv line added

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
-require('dotenv').config()
+import axios from "axios"
+require("dotenv").config()
 
-const API = axios.create({ baseURL: REACT_APP_API_BASEURL })
+const API = axios.create({ baseURL: process.env.REACT_APP_API_BASEURL })
 
-API.interceptors.request.use(req => {
-	if (localStorage.getItem('profile')) {
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem("profile")) {
 		req.headers.Authorization = `Bearer ${JSON.parse(
-			localStorage.getItem('profile').token
+			localStorage.getItem("profile").token
 		)}`
 
 		return req
@@ -14,10 +14,10 @@ API.interceptors.request.use(req => {
 })
 
 // user management endpoints requests
-export const signin = formdata => API.post('/login', formdata)
-export const signup = formdata => API.post('/create', formdata)
+export const signin = (formdata) => API.post("/login", formdata)
+export const signup = (formdata) => API.post("/create", formdata)
 export const updateUser = (formdata, id) =>
 	API.patch(`/update/user/:${id}`, formdata)
-export const deleteUser = id => API.delete(`/delete/:${id}`)
+export const deleteUser = (id) => API.delete(`/delete/:${id}`)
 
 // blood management endpoints requests
