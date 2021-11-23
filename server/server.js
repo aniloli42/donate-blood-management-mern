fcb561101add5d8553404b7d4aa5b6a583d30838
aniloli42 <aniloli42@gmail.com>
2021-11-24 00:25:54 +0545

    chore: fix intial path for server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
 	res.status.json({ message: "Welcome To Donate Blood Server" })
 })
 
