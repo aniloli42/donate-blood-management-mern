4aed79ab26166e09395b26fd1de283cc16926feb
aniloli42 <aniloli42@gmail.com>
2021-12-23 18:42:22 +0545

    chore: make keep server alive

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-	res.status(200).json({ message: "Welcome To Donate Blood Server" })
+  res.status(200).json({ message: "Welcome To Donate Blood Server" })
 })
 
 const port = process.env.PORT || 4001
@@ -20,11 +20,11 @@ const url = process.env.DB_URL
 moongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 moongoose.connection.on("error", (err) => {
-	console.log(err)
+  console.log(err)
 })
 
 moongoose.connection.once("open", () => {
-	console.log("Connected to database")
+  console.log("Connected to database")
 })
 
 //  imports
@@ -33,6 +33,7 @@ const auth = require("./routes/auth")
 const profile = require("./routes/profile")
 const history = require("./routes/history")
 const requests = require("./routes/requests")
+const { default: axios } = require("axios")
 
 // routes
 app.use("/otp", otp)
@@ -42,5 +43,19 @@ app.use("/history", history)
 app.use("/requests", requests)
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
+
+// for keep server live on heroku
+
+const keepLive = async () => {
+  try {
+    await axios.get("https://donate-blood-server.herokuapp.com/")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+setInterval(() => {
+  keepLive()
+}, 1800000)
