05d0571b4cbd92c1b2a3ae384835cea7525305a2
aniloli42 <aniloli42@gmail.com>
2022-12-05 17:15:30 +0545

    maintenance mode on

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,105 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import "./Styles/App.css";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './Styles/App.css'
 
 import {
-  Signup,
-  Login,
-  ForgetPassword,
-  Dashboard,
-  Profile,
-  Security,
-  Requests,
-  DonationHistory,
-} from "./Pages";
+	Dashboard,
+	DonationHistory,
+	ForgetPassword,
+	Login,
+	Profile,
+	Requests,
+	Security,
+	Signup,
+} from './Pages'
 
-import NotFound from "./Components/NotFound/NotFound";
-import { Loader, MessageCard } from "./Components";
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux'
+import { Loader, MessageCard } from './Components'
+import NotFound from './Components/NotFound/NotFound'
 
 function App() {
-  const { status: loaderStatus } = useSelector((state) => state.Loader);
+	const { status: loaderStatus } = useSelector((state) => state.Loader)
 
-  return (
-    <>
-      {loaderStatus && <Loader />}
-      <MessageCard />
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/profile/:id" element={<Profile />} />
-        <Route exact path="/security" element={<Security />} />
-        <Route exact path="/requests" element={<Requests />} />
-        <Route exact path="/donation-history" element={<DonationHistory />} />
+	const divStyle = {
+		position: 'absolute',
+		backgroundColor: '#00000050',
+		inset: 0,
+		zIndex: '10',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		userSelect: 'none',
+	}
 
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
+	const noticeTextStyle = {
+		fontSize: '5vw',
+		color: '#fffaff',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	}
 
-        <Route exact path="/forget-password" element={<ForgetPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<div style={divStyle}>
+				<div
+					style={{
+						display: 'flex',
+						padding: '2rem',
+						backgroundColor: '#010',
+						flexDirection: 'column',
+						borderRadius: 'vw',
+						gap: '5vw',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<div
+						aria-hidden='true'
+						style={{
+							height: '14vw',
+							width: '14vw',
+							backgroundColor: '#cc0000',
+							borderRadius: '1.5vw',
+							border: '.5vw solid #fff',
+							fontSize: '10vw',
+							color: '#fff',
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							// rotate: '45deg',
+						}}
+					>
+						!
+					</div>
+					<p style={noticeTextStyle}>Under Maintenance</p>
+				</div>
+			</div>
+			{loaderStatus && <Loader />}
+			<MessageCard />
+			<Routes>
+				<Route exact path='/' element={<Dashboard />} />
+				<Route exact path='/profile' element={<Profile />} />
+				<Route exact path='/profile/:id' element={<Profile />} />
+				<Route exact path='/security' element={<Security />} />
+				<Route exact path='/requests' element={<Requests />} />
+				<Route
+					exact
+					path='/donation-history'
+					element={<DonationHistory />}
+				/>
+
+				<Route exact path='/login' element={<Login />} />
+				<Route exact path='/signup' element={<Signup />} />
+
+				<Route
+					exact
+					path='/forget-password'
+					element={<ForgetPassword />}
+				/>
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</>
+	)
 }
 
-export default App;
+export default App
