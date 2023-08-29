d54558ec1d9259e9183e638bf6f4215cf7061b6d
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-08-29 06:32:21 +0545

    expose port from dockerfile, add env example

--- server/server.js
+++ server/server.js
@@ -1,47 +1,47 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const moongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const moongoose = require("mongoose");
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome To Donate Blood Server' })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome To Donate Blood Server" });
+});
 
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 5000;
 
-const url = process.env.DB_URL
+const url = process.env.DB_URL;
 
-moongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+moongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-moongoose.connection.on('error', (err) => {
-  console.log(err)
-})
+moongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
-moongoose.connection.once('open', () => {
-  console.log('Connected to database')
-})
+moongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
 
 //  imports
-const otp = require('./routes/otp')
-const auth = require('./routes/auth')
-const profile = require('./routes/profile')
-const history = require('./routes/history')
-const requests = require('./routes/requests')
-const axios = require('axios')
+const otp = require("./routes/otp");
+const auth = require("./routes/auth");
+const profile = require("./routes/profile");
+const history = require("./routes/history");
+const requests = require("./routes/requests");
+const axios = require("axios");
 
 // routes
-app.use('/otp', otp)
-app.use('/auth', auth)
-app.use('/profile', profile)
-app.use('/history', history)
-app.use('/requests', requests)
+app.use("/otp", otp);
+app.use("/auth", auth);
+app.use("/profile", profile);
+app.use("/history", history);
+app.use("/requests", requests);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
