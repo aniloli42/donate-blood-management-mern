1163996fe8282fb2b71da81e4d7213f50f790a40
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-08-29 06:47:06 +0545

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,106 +1,47 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './styles/App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./styles/App.css";
 
 import {
-    Dashboard,
-    DonationHistory,
-    ForgetPassword,
-    Login,
-    Profile,
-    Requests,
-    Security,
-    Signup
-} from './pages'
+  Dashboard,
+  DonationHistory,
+  ForgetPassword,
+  Login,
+  Profile,
+  Requests,
+  Security,
+  Signup,
+} from "./pages";
 
-import { useSelector } from 'react-redux'
-import { Loader, MessageCard } from './components'
-import NotFound from './components/NotFound/NotFound'
+import { useSelector } from "react-redux";
+import { Loader, MessageCard } from "./components";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-	const { status: loaderStatus } = useSelector((state) => state.Loader)
+  const { status: loaderStatus } = useSelector((state) => state.Loader);
 
-	const divStyle = {
-		position: 'absolute',
-		backgroundColor: '#00000050',
-		inset: 0,
-		zIndex: '10',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		userSelect: 'none',
-	}
+  return (
+    <>
+      {loaderStatus && <Loader />}
+      <MessageCard />
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route path="/profile">
+          <Route index element={<Profile />} />
+          <Route path=":id" element={<Profile />} />
+        </Route>
+        <Route exact path="/security" element={<Security />} />
+        <Route exact path="/requests" element={<Requests />} />
+        <Route exact path="/donation-history" element={<DonationHistory />} />
 
-	const noticeTextStyle = {
-		fontSize: '5vw',
-		color: '#fffaff',
-		fontWeight: 'bold',
-		textAlign: 'center',
-	}
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
 
-	return (
-		<>
-			<div style={divStyle}>
-				<div
-					style={{
-						display: 'flex',
-						padding: '2rem',
-						backgroundColor: '#010',
-						flexDirection: 'column',
-						borderRadius: 'vw',
-						gap: '5vw',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<div
-						aria-hidden='true'
-						style={{
-							height: '14vw',
-							width: '14vw',
-							backgroundColor: '#cc0000',
-							borderRadius: '1.5vw',
-							border: '.5vw solid #fff',
-							fontSize: '10vw',
-							color: '#fff',
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-						}}
-					>
-						!
-					</div>
-					<p style={noticeTextStyle}>Under Maintenance</p>
-				</div>
-			</div>
-			{loaderStatus && <Loader />}
-			<MessageCard />
-			<Routes>
-				<Route exact path='/' element={<Dashboard />} />
-				<Route path='/profile'>
-					<Route index element={<Profile />} />
-					<Route path=':id' element={<Profile />} />
-				</Route>
-				<Route exact path='/security' element={<Security />} />
-				<Route exact path='/requests' element={<Requests />} />
-				<Route
-					exact
-					path='/donation-history'
-					element={<DonationHistory />}
-				/>
-
-				<Route exact path='/login' element={<Login />} />
-				<Route exact path='/signup' element={<Signup />} />
-
-				<Route
-					exact
-					path='/forget-password'
-					element={<ForgetPassword />}
-				/>
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</>
-	)
+        <Route exact path="/forget-password" element={<ForgetPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { BrowserRouter as Router } from "react-router-dom"
-import { applyMiddleware, createStore } from "redux"
-import thunk from "redux-thunk"
-import reducers from "./reducers/index.js"
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/index.js";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-import App from "./App"
+import App from "./App";
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</Router>
-	</React.StrictMode>,
-	document.getElementById("root")
-)
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
