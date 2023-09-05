71edd17d3e3d03c49165ba42906537e0834b649d
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-09-05 13:30:17 +0545

    fix: package update import fix

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers/index.js'
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 import App from './App'
 
